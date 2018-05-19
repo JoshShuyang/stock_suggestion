8472b8977f9bcb3f15eb411d3a02263e3ae039ad
Shuyang Yu <graneyysy@gmail.com>
2018-05-19 16:56:26 -0700

    minor text fix

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -9,10 +9,9 @@ jQuery(document).ready(function($) {
         transitionSpeed: 400
     });
 
-    let crazy = window.crazy ? window.crazy : {};
-    crazy.selectedStrategies = [];
+    let strategyChoices = [];
 
-    $(document).on("keyup", "#input_amount", function(){
+    $(document).on("keyup", "#stack_value", function(){
       toogleHintAndSubmitBtn();
     });
 
@@ -22,25 +21,25 @@ jQuery(document).ready(function($) {
 
         // update selected strategies
         let key = $(this).find("h4").text();
-        let keyIndex = crazy.selectedStrategies.indexOf(key);
+        let keyIndex = strategyChoices.indexOf(key);
         if(keyIndex < 0)
         {
-          crazy.selectedStrategies.push(key);
+          strategyChoices.push(key);
         }
         else
         {
-          crazy.selectedStrategies.splice(keyIndex, 1);
+          strategyChoices.splice(keyIndex, 1);
         }
 
         toogleHintAndSubmitBtn();
 
-        console.log(crazy.selectedStrategies);
+        console.log(strategyChoices);
     });
 
-    $(document).on("click", "#submit_strategy_selection", function(){
+    $(document).on("click", "#get_suggestion", function(){
 
         let $form = $("#strategy_selection_form");
-        $form.find("#selected_strategies_val").val(JSON.stringify(crazy.selectedStrategies));
+        $form.find("#selected_strategies_val").val(JSON.stringify(strategyChoices));
         $form.submit();
 
     });
@@ -49,29 +48,29 @@ jQuery(document).ready(function($) {
  function toogleHintAndSubmitBtn()
   {
     let $selection_hint = $("#selection_hint");
-    let $submit_btn_container = $("#submit_btn_container");
+    let $suggestion_btn_container = $("#suggestion_btn_container");
     if(validStratrgiesInput())
     {
       $selection_hint.hide();
-      $submit_btn_container.show();
+      $suggestion_btn_container.show();
     }
     else
     {
       $selection_hint.show();
-      $submit_btn_container.hide();
+      $suggestion_btn_container.hide();
     }
   }
 
   function validStratrgiesInput()
   {
-    let inputAmount = $("#input_amount").val();
-    if(!inputAmount || inputAmount.length < 1)
+    let stackValue = $("#stack_value").val();
+    if(!stackValue || stackValue.length < 1)
     {
       return false;
     }
-    if(crazy.selectedStrategies.length > 2
-      || crazy.selectedStrategies.length <= 0
-      || parseFloat(inputAmount) < 5000)
+    if(strategyChoices.length > 2
+      || strategyChoices.length <= 0
+      || parseFloat(stackValue) < 5000)
     {
       return false;
     }
